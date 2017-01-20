504371eac18d23730d9eec0e18c78b9343bddefe
Wosker4yan <wosker4yan@gmail.com>
2017-01-20 18:16:41 +0400

    Chapter 15th from Deitel  (Programming in Java)

---
+++ src/DEITEL/Chapter15/DrawPolygons.java
@@ -0,0 +1,18 @@
+package DEITEL.Chapter15;
+// Fig. 15.28: DrawPolygons.java
+// Drawing polygons.
+import javax.swing.JFrame;
+public class DrawPolygons
+{
+    // execute application
+    public static void main( String[] args )
+    {
+// create frame for PolygonsJPanel
+        JFrame frame = new JFrame( "Drawing Polygons" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        PolygonsJPanel polygonsJPanel = new PolygonsJPanel();
+        frame.add( polygonsJPanel ); // add polygonsJPanel to frame
+        frame.setSize( 280, 270 ); // set frame size
+        frame.setVisible( true ); // display frame
+    } // end main
+} // end class DrawPolygons
---
+++ src/DEITEL/Chapter15/ShowColors2.java
@@ -0,0 +1,14 @@
+package DEITEL.Chapter15;
+
+// Fig. 15.8: ShowColors2.java
+// Choosing colors with JColorChooser.
+import javax.swing.JFrame;
+public class ShowColors2
+{
+    // execute application
+    public static void main( String[] args )
+    {
+        ShowColors2JFrame application = new ShowColors2JFrame();
+        application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    } // end main
+} // end class ShowColors2
